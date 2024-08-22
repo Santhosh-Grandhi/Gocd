eeaf0cdbbee81853fcac825a2f1ba6967cdad8f0
SanthoshGrandhi <santhoshgrandhi89@gmail.com>
2024-08-22 23:33:27 +0530

    GoCd Server changes

--- GoCdServer.sh
+++ GoCdServer.sh
@@ -1,7 +1,7 @@
 dnf install java-17-openjdk.x86_64 -y
-useradd go
-curl -L -o /tmp/go-server-23.5.0-18179.zip  https://download.gocd.org/binaries/23.5.0-18179/generic/go-server-23.5.0-18179.zip
-su - gocd -c 'unzip /tmp/go-server-23.5.0-18179.zip'
+useradd gocd
+curl -L -o /tmp/go-server.zip  https://download.gocd.org/binaries/23.5.0-18179/generic/go-server.zip
+su - gocd -c 'unzip /tmp/go-server.zip'
 cp gocd-server.service /etc/systemd/system/gocd-server.service
 systemctl daemon-reload
 systemctl enable gocd-server
