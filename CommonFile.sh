b7138ee9522a3ceece230321cf2706f0c1b1e956
SanthoshGrandhi <santhoshgrandhi89@gmail.com>
2024-08-24 13:19:05 +0530

    GoCd Server-bash scripting

---
+++ CommonFile.sh
@@ -0,0 +1,13 @@
+Heading() {
+  echo -e "\e[35m$*\e[0m"
+}
+
+STAT() {
+  if [ "$1" -eq 0 ]; then
+    echo Success
+  else
+    echo Failed
+    exit 2
+    fi
+   }
+
--- GoCdServer.sh
+++ GoCdServer.sh
@@ -1,8 +1,31 @@
+Heading "Install Java"
 dnf install java-17-openjdk.x86_64 -y
-useradd gocd
+STAT $?
+
+Heading "Add User"
+id gocd
+if [ $? -ne 0 ]; then
+  useradd gocd
+fi
+STAT $?
+
+Heading "Download Gocd Server"
 curl -L -o /tmp/go-server.zip  https://download.gocd.org/binaries/23.5.0-18179/generic/go-server.zip
+STAT $?
+
+Heading "Unzip downloaded file using above created user"
 su - gocd -c 'unzip /tmp/go-server.zip'
+STAT $?
+
+Heading "Copy Gocd Service file"
 cp gocd-server.service /etc/systemd/system/gocd-server.service
+STAT $?
+
+Heading "Reload Deamon"
 systemctl daemon-reload
+STAT $?
+
+Heading "Enable and Start Service"
 systemctl enable gocd-server
 systemctl start gocd-server
+STAT $?
